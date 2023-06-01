f7ecac36f0faf77e7626cdf86a4be05e90312607
David Bowles <david.bowles84272@outlook.com>
2023-06-01 19:11:41 +0000

    javascript function questionChosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
  * This welcome timer allows users to see the title, read the title and header Descrition
  */
 function welcomeTimer() {
-    setTimeout(welcomeAlert, 1000);
+    setTimeout(welcomeAlert, 10);
 }
 
 /**
@@ -19,4 +19,35 @@ function welcomeAlert() {
     document.getElementById("big-number").focus();
 }
 
+/**
+ * This function runs once the complete this sum btn is clicked
+ * main part of this function is to change the form inputs into styled text remove the btns from div and create a new btn
+ */
+function questionChosen() {
+    let chosenBigNumber = document.getElementById("big-number").value;
+    let chosenLittleNumber = document.getElementById("big-number").value;
+    let lockedIn = document.getElementById("input-to-locked-in");
+    let newBtn = document.createElement("button");
+
+    // add attributes and button text for the new refresh button
+    newBtn.setAttribute("id", "refresh-answer-panel-btn");
+    newBtn.innerHTML = "Refresh and start a new sum";
+
+    // changes the form elements into p elements so user can see what they have chosen
+    lockedIn.innerHTML = `<p class="padding-top-bottom"><span class="big-number text-shadow">` + chosenBigNumber + `</span><span class="little-number text-shadow">` + chosenLittleNumber + `</span></p>
+    
+    
+
+    
+    <p>The sum you have chosen is ` + chosenBigNumber + ` to the power of ` + chosenLittleNumber + `</p>`;
+
+    // removes buttons that are no longer appropriate
+    document.getElementById("question-tile-calculate-btn").remove();
+    document.getElementById("question-tile-random-btn").remove();
+
+    // Adds the new button to be able to refresh the sum/panel if user no longer wants to complete the current sum
+    lockedIn.appendChild(newBtn);
+}
+
 document.addEventListener("DOMContentLoaded", welcomeTimer);
+document.getElementById("question-tile-calculate-btn").addEventListener("click", questionChosen);
